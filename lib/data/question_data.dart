705f0b6139583dfab48e1e05be7ecf3bdade4bd5
Sandy McMillan <sandy_mcmillan@yahoo.co.uk>
2021-06-08 16:05:43 +0100

    can select more than one answer

--- lib/data/question_data.dart
+++ lib/data/question_data.dart
@@ -2,16 +2,17 @@ import 'package:cc_quiz_app/models/question.dart';
 
 final question1 = Question('What do we mean by conditional logic?',
     'Checking if a condition is true or false before running a section of code',
-    ['Code that sorts an array into alphabetical or numerical order',
+    ['Checking if a condition is true or false before running a section of code',
+          'Code that sorts an array into alphabetical or numerical order',
       'When our project is dependent on other libraries to allow it to work']);
 
-final question2 = Question('How do we check equality?', '==', ['=', '&&', '||']);
+final question2 = Question('How do we check equality?', '==', [ '==', '=', '&&', '||']);
 final question3 = Question('What do we use functions for?',
 'To hold small chunks of code that can be invoked later on in our program',
-['To hold lots of code that does many things which can be invoked later on in our program', 'To hold code we aren’t sure we want to use just yet']);
+['To hold small chunks of code that can be invoked later on in our program', 'To hold lots of code that does many things which can be invoked later on in our program', 'To hold code we aren’t sure we want to use just yet']);
 final question4 = Question('How many arguments can be passed into a function?', 'As many arguments that the function is expecting',
-['One argument only', 'As many as we like']);
-final question5 = Question('How many parameters can be set for a function?', 'As many as we need', ['One parameter only', 'A maximum of 10 parameters']);
+['As many arguments that the function is expecting', 'One argument only', 'As many as we like']);
+final question5 = Question('How many parameters can be set for a function?', 'As many as we need', [ 'As many as we need', 'One parameter only', 'A maximum of 10 parameters']);
 
 List<Question> basicQuestions = [question1, question2, question3, question4, question5];
 
